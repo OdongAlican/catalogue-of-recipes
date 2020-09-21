fe5f2fb00a746f4108abf466e61125763e1f1232
OdongAlican <sandieo.2020@gmail.com>
2020-09-22 00:49:06 +0300

    add API call using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { FETCH_RECIPIES_REQUEST , FETCH_RECIPIES_SUCCESS, FETCH_RECIPIES_FAILURE } from '../actions/actionTypes'
 
 
@@ -18,5 +19,18 @@ export const fetchRecipiesFailure = error => {
     return {
         type: FETCH_RECIPIES_FAILURE,
         payload: error
+    }
+}
+
+export const fetchRecipies = () => {
+    return (dispatch) => {
+        dispatch(fetchRecipiesRequest)
+        axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+                const recipies = response.data;
+                dispatch(fetchRecipiesSuccess(recipies))
+            }).catch(error => {
+                const errorMsg = error.message
+                dispatch(fetchRecipiesFailure(errorMsg))
+            })
     }
 }
