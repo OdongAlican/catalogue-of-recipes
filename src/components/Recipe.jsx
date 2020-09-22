c8fc782b70a4cda0ef4516f2a80a7f7e6601cf72
OdongAlican <sandieo.2020@gmail.com>
2020-09-22 12:02:07 +0300

    update recipe displayed content

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -5,11 +5,12 @@ function Recipe({ recipe }){
     return (
         <div>
             <div className="card col-md-3 mx-2 mb-2" >
+            <h4>{ recipe.id }</h4>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">{ recipe.name }</li>
-                    <li className="list-group-item"> { recipe.username }</li>
-                    <li className="list-group-item"> { recipe.email } </li>
+                    <li className="list-group-item"> { recipe.title }</li>
+                    <li className="list-group-item"> { recipe.userId }</li>
                 </ul>
+                <p>{ recipe.body}</p>
             </div>
         </div>
     )
