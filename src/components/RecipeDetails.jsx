d01b53abf0775b5d300d4b16c051a90d992dcf8c
OdongAlican <sandieo.2020@gmail.com>
2020-09-23 13:15:23 +0300

    update RecipeDetail component

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -5,16 +5,25 @@ import { Link } from 'react-router-dom'
 const RecipeDetails = (props) => {
 
     const recipeData = props.location.state.recipe
+    const dateArray = recipeData.publishedDate.split(' ')
+    const dateValue = new Date(dateArray[0])
 return (
     <div>
-        <div className="card p-2 col-6 border" >
-            <div className="card-body">
-                <h5 className="card-title"> {recipeData.title} </h5>
-                <p className="card-text"> {recipeData.title} </p>
-               
+        <div className="card p-2 col-6 border mx-auto mt-3 border" >
+        <div  className="image-section-details mx-auto mb-2 border p-2">
+                    <img src={recipeData.image} alt="boohoo" className="image-detail"/>
+                </div>
+            <div className="card-body card-section">
+                <h5 className="card-title text-underline"> {recipeData.title} </h5>
+                <p className="card-text text-secondary">{ recipeData.text }</p>
+                <p className="card-text text-underline"> Published Date: {
+                dateValue.toDateString()} </p>
+                <p className="card-text text-underline"> Web-site:                     
+                <a href={recipeData.site} target = '_blank' className="ml-1">  {recipeData.site}</a>
+                </p>
             </div>
             <button className="col-md-2 btn btn-primary">
-                <Link to = {{ pathname: '/'}}>
+                <Link to = {{ pathname: '/'}} className="text-white">
                      Home 
                 </Link>
             </button>
