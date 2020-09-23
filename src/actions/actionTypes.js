521dec094f19ed9b9b478f57713c76e49a0c8ea1
OdongAlican <sandieo.2020@gmail.com>
2020-09-23 07:22:48 +0300

    update content url

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -2,3 +2,5 @@ export const FETCH_RECIPIES_REQUEST = 'FETCH_RECIPIES_REQUEST'
 export const FETCH_RECIPIES_SUCCESS = 'FETCH_RECIPIES_SUCCESS'
 export const FETCH_RECIPIES_FAILURE = 'FETCH_RECIPIES_FAILURE'
 export const FILTER_RECIPE = 'FILTER_RECIPE'
+const API_TOKEN = '7ab5565ace50dd292b7cfd5bd4ae89d9'
+export const URL = `https://financialmodelingprep.com/api/v3/stock_news?tickers=AAPL,FB,GOOG,AMZN&limit=50&apikey=${API_TOKEN}`
