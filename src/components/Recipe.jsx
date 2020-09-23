c0a01f8185b6ce728f9d61d2f8e557414cbd9c87
OdongAlican <sandieo.2020@gmail.com>
2020-09-23 07:24:56 +0300

    add styles

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom'
 
 function Recipe({ recipe }){
     return (
-        <div className="mx-auto col-md-4">
-            <div className="card col-md-12 mx-2 mb-2" >
-            <h4>{ recipe.id }</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item"> { recipe.title }</li>
-                    <li className="list-group-item"> { recipe.userId }</li>
-                </ul>
-                <p>{ recipe.body}</p>
+        <div className="col-md-3 bg-warning ">
+            <div className="card col-md-12 mx-2 mb-2 p-4" >
+                <div  className="image-section mx-auto mb-2">
+                    <img src={recipe.image} alt="boohoo" className="image-det"/>
+                </div>
+            <h5>{ recipe.title }</h5>
+                <p>Symbol: { recipe.symbol}</p>
+                <label htmlFor="site">Visit Us:
+                <a href={recipe.site} target = '_blank'> { recipe.site }</a>
+                </label>
                 <button className="btn btn-primary mb-1">
-                    <Link to={{ 
-                        pathname: `/recipe/${recipe.id}`,
+                    <Link className="text-white" to={{ 
+                        pathname: `/recipe/${recipe.title}`,
                         state: { recipe } 
                         }}>View Recipe</Link>
                 </button>
