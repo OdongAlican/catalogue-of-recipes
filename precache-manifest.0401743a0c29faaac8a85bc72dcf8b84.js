c43578571704045ec1c7078a01d4791609b6a9ac
OdongAlican <sandieo.2020@gmail.com>
2020-10-26 01:05:30 +0300

    Updates

---
+++ precache-manifest.0401743a0c29faaac8a85bc72dcf8b84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fe23e8a2a9c28daa854dd0667df6a6",
+    "url": "/catalogue-of-recipes/index.html"
+  },
+  {
+    "revision": "96f3e0cd79efa42e92b7",
+    "url": "/catalogue-of-recipes/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "556ef866ef0ae82118dd",
+    "url": "/catalogue-of-recipes/static/css/main.c9af426b.chunk.css"
+  },
+  {
+    "revision": "96f3e0cd79efa42e92b7",
+    "url": "/catalogue-of-recipes/static/js/2.905d1fad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/catalogue-of-recipes/static/js/2.905d1fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556ef866ef0ae82118dd",
+    "url": "/catalogue-of-recipes/static/js/main.3440931f.chunk.js"
+  },
+  {
+    "revision": "45290254d71c1b7235e9",
+    "url": "/catalogue-of-recipes/static/js/runtime-main.b9782b9c.js"
+  }
+]);
